ee86c7222fd481247cac60dc9f24629931a0943a
Robert <robert.blomdalen@gmail.com>
2013-02-13 00:28:05 +0100

    Återställer efter medge

--- fetch.js
+++ fetch.js
@@ -7,19 +7,24 @@ var energiLank;
 var casper = require('casper').create(
 {
 	clientScripts: ["jquery-1.9.0.min.js"]
-//    verbose: true,
-//    logLevel: "debug"
+    //verbose: true,
+    //logLevel: "debug"
 }
 );
+phantom.cookiesEnabled = true;
 if(!(casper.cli.has(0) && casper.cli.has(1) && casper.cli.has('anlid'))) {
-	casper.echo("Ange anläggningsid, användarnamn och lösenord som argument till scriptet. Ex. casperjs fetch.js namn lösen --anlid=1234567890").exit();
+	casper.echo("\nAnge anläggningsid, användarnamn och lösenord som argument till scriptet.\nEx. casperjs fetch.js namn lösen --anlid=1234567890\n\nFick:\n"+casper.cli.get(0)+"\n"+casper.cli.get(1)+"\n"+casper.cli.raw.get('anlid')).exit();
 }
 var anlId = casper.cli.raw.get('anlid');//Får inte hela anlid med bara enkel get...
 var anvNamn = casper.cli.get(0);
 var losenord = casper.cli.get(1);
 var now = new Date();
-var startDag = now.getDate() -1; // Day of the month
-var dagLank;
+var startDag = now.getDate() -1;
+var viewstate;
+var ctl00$cphBody$ScriptManager1;
+var eventvalidation;
+var ctl00$cphBody$txtSessionTimeOut;
+var ctl00$cphBody$ucYearPicker$txtYear;
 
 casper.options.viewportSize = {width: 1280, height: 673};
 casper.start('https://gbgc.goteborgenergi.se/main/default.asp');
@@ -69,8 +74,7 @@ casper.waitForSelector(x("//a[normalize-space(text())='Din elanvändning']"),
 casper.then(function() {
 	energiLank = this.getElementAttribute("a[target='StatisticsWin']", "href");
     this.echo("LÄNK: " + energiLank);
-	// @todo Skapa funktionalitet för att validera om länken är riktig, på så sätt kan man återanvända länken ifall den är giltig utan att behöva logga in via den första sidan
-    casper.open(energiLank);
+	casper.open(energiLank);
 });
 casper.waitForSelector(x("//a[normalize-space(text())='"+ anlId +"']"),
     function success() {
@@ -82,34 +86,59 @@ casper.waitForSelector(x("//a[normalize-space(text())='"+ anlId +"']"),
 });
 casper.waitForSelector("#cphBody_lbDay",
     function success() {
-        this.test.assertExists("#cphBody_lbDay", "Dagslänk finns");
-        dagLank = this.evaluate(function() {
-            return __utils__.findOne('#cphBody_lbDay').getAttribute('href');
-        });
-
-        this.evaluate(function() {
-            // Alternativet mot att "klicka" på länken är att lägga till lyssnare på ajax-svaret, där finns tabellen i klartext
-            eval(dagLank);
-        });
-        // this.click("#cphBody_lbDay");//Misslyckas varje gång...
+        this.test.assertExists("#cphBody_lbDay");
+		viewstate = this.evaluate(function() {
+			return $('input[name="__VIEWSTATE"]').val();
+		});
+		eventvalidation = this.evaluate(function() {
+			return $('input[name="__EVENTVALIDATION"]').val();
+		});
+		ctl00$cphBody$ScriptManager1 = this.evaluate(function() {
+			return $('input[name="ctl00$cphBody$ScriptManager1"]').val();
+		});
+		ctl00$cphBody$txtSessionTimeOut = this.evaluate(function() {
+			return $('input[name="ctl00$cphBody$txtSessionTimeOut"]').val();
+		});
+		ctl00$cphBody$ucYearPicker$txtYear = this.evaluate(function() {
+			return $('input[name="ctl00$cphBody$ucYearPicker$txtYear"]').val();
+		});
+		//this.echo('__EVENTVALIDATION'+eventvalidation);
+		//this.echo('ctl00$cphBody$ScriptManager1 '+ctl00$cphBody$ScriptManager1);
+		//this.echo('ctl00$cphBody$txtSessionTimeOut '+ctl00$cphBody$txtSessionTimeOut);
+		//this.echo('ctl00$cphBody$ucYearPicker$txtYear '+ctl00$cphBody$ucYearPicker$txtYear);
+		var url_now = this.getCurrentUrl();
+		this.echo("Försöker posta mot url: "+url_now);
+		this.evaluate(function(url) {
+			$.ajax({
+				url: url_now,
+				type: 'POST',
+				data: { 
+						'__EVENTTARGET': 'ctl00%24cphBody%24lbDay',  
+						'__EVENTARGUMENT': '', 
+						'__VIEWSTATE': viewstate, 
+						'__EVENTVALIDATION': eventvalidation, 
+						'ctl00$cphBody$ucYearPicker$txtYear': ctl00$cphBody$ucYearPicker$txtYear,
+						'ctl00$cphBody$txtSessionTimeOut': ctl00$cphBody$txtSessionTimeOut, 
+						'ctl00$cphBody$ScriptManager1': 'ctl00%24cphBody%24pnlUpdatePanel%7Cctl00%24cphBody%24lbDay', 
+						'__ASYNCPOST': 'true'
+						}
+			}).done(function(data){__utils__.echo(data);})
+			.fail(function() { __utils__.echo("error"); })
+			.always(function() { __utils__.echo("finished"); });;
+		}, url_now);
+		this.wait(5000);
     },
     function fail() {
         this.test.assertExists("#cphBody_lbDay");
 });
 casper.wait(5000);
-
-// Här kommer nu tabellen över dagar att treverseras
-// @todo Hämta antal möjliga dagar
-// Läs markerad
-// Klicka på "nästkommande"
-casper.waitForSelector(".existingValue.selectedDay",
+casper.waitForSelector("#cphBody_UcDailyPicker_lnkDay"+(startDag+1),
     function success() {
-        this.test.assertExists(".existingValue.selectedDay");
-        // Klick skall endast ske i nästa iteration
-        // this.click(".existingValue.selectedDay"));
+        this.test.assertExists("#cphBody_UcDailyPicker_lnkDay"+(startDag+1));
+        this.click("#cphBody_UcDailyPicker_lnkDay"+(startDag+1));
     },
     function fail() {
-        this.test.assertExists(".existingValue.selectedDay");
+        this.test.assertExists("#cphBody_UcDailyPicker_lnkDay"+(startDag+1));
 });
 casper.wait(1000);
 casper.then(function() {
